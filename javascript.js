cc85dfdc824acca3de4a232f04449d9917f3ac26
user.email <developerjenzii@gmail.com>
2022-07-18 23:22:39 +0200

    Add drawing functionality and reset

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 function createBoxedLayout(){
+  document.body.innerText = '';
   const container = document.createElement('div');
   container.classList.add('container');
   document.body.appendChild(container);
-  
   for ( let i = 0; i < 256; i++){
     const div = document.createElement('div');
     let divGrid = container.appendChild(div);
     div.style = 'background-color:pink;';
      divGrid += divGrid;
   }
-  
 }
-//document.addEventListener('click', createBoxedLayout);
+
+function changeColor(){
+  this.style = 'background-color:gold;';
+}
+
 createBoxedLayout();
+document.querySelector('body').addEventListener('mouseup', createBoxedLayout);
+document.querySelectorAll('.container div').forEach(element => element.addEventListener('mouseover', changeColor));
