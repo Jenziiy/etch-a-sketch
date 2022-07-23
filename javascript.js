76a442c7ca89f7108816990fdb8e725a197b0e86
user.email <developerjenzii@gmail.com>
2022-07-23 15:52:31 +0200

    Add look n feel & refactoring - comes with bugs :/

--- javascript.js
+++ javascript.js
@@ -1,20 +1,44 @@
-function createBoxedLayout(){
-  document.body.innerText = '';
-  const container = document.createElement('div');
-  container.classList.add('container');
-  document.body.appendChild(container);
-  for ( let i = 0; i < 256; i++){
-    const div = document.createElement('div');
-    let divGrid = container.appendChild(div);
-    div.style = 'background-color:pink;';
-     divGrid += divGrid;
+import { button, buttonClear, modal, span, defaultSizeSquared, maxInput, initializePad, getContainer, input } from './utils.js';
+let userInput = 10;
+function createSketchPad(userInput_c){
+  initializePad();
+if (userInput_c > maxInput) userInput = defaultSizeSquared; // Setting a default value to prevent freezing, crashing & delays.
+  for ( let i = 0; i < userInput_c **2; i++){
+    getContainer;
+    const createSquare = document.createElement('div');
+    getContainer.append(createSquare);
+    createSquare.classList.add('square');
+    let sketchGrid = getContainer.appendChild(createSquare);
+    createSquare.style = 'background-color:pink;';
+     sketchGrid += sketchGrid;
   }
+  getContainer.style.gridTemplateColumns = `repeat(${userInput},1fr)`;
+}
+
+function getInputValue() {
+  let userInput_c = document.getElementById('input-tiles').value;
+  alert(userInput_c);
+  //userInput = userInput.value;
+  input.addEventListener('submit', () => modal.style.display = 'none');
+  createSketchPad(userInput_c);
+  return userInput;
+
+}
+
+function draw() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square=>square.addEventListener("mouseover", changeColor));
 }
 
 function changeColor(){
-  this.style = 'background-color:gold;';
+  this.style.backgroundColor = 'gold';
 }
+createSketchPad(userInput);
+input.addEventListener('submit', getInputValue);
+button.addEventListener('click', () => modal.style.display = 'block');
+span.addEventListener('click', () => modal.style.display = 'none');
+span.addEventListener('click', createSketchPad);
+getContainer.addEventListener('mousedown', draw);
+buttonClear.addEventListener('click', createSketchPad);
 
-createBoxedLayout();
-document.querySelector('body').addEventListener('mouseup', createBoxedLayout);
-document.querySelectorAll('.container div').forEach(element => element.addEventListener('mouseover', changeColor));
+
